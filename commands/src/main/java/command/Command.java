f143faab45462e44c5e0094a857a3d06f21ba432
Mikhail-Bunkov <bunko04@bk.ru>
2021-06-01 00:42:45 +1000

    Initial commit

---
+++ commands/src/main/java/command/Command.java
@@ -0,0 +1,21 @@
+package command;
+
+public class Command {
+	public static final Integer PORT = 8008;
+	public static final String HOST = "localhost";
+	public static final String CLOUD = "cloud";
+	public static final String AUTH = "auth";
+	public static final String OK = "ok";
+	public static final String WRONG = "wrong";
+	public static final String CAT_COMMAND = "cat";
+	public static final String TOUCH_COMMAND = "touch";
+	public static final String MAKE_DIRECTORY= "mkdir";
+	public static final String RM_COMMAND = "rm";
+	public static final String ROOT = "root";
+	public static final String LS = "ls";
+	public static final String CHANGE_DIRECTORY = "cd";
+	public static final String UP_MARK ="..";
+	public static final String DOWNLOAD_COMMAND = "download";
+	public static final String EXIT_COMMAND ="exit";
+//	public static final String FILE_INFO ="fileinfo";
+}
